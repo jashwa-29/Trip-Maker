81f04c0a71e2f13c968c48c9d52cb46f9c10e17c
Jashwa <jashwa46733@gmail.com>
2024-07-17 13:05:51 +0530

    Update Navbar.jsx

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -35,21 +35,37 @@ const Navbar = () => {
       <div className='flex justify-between items-center w-[83%] mx-auto py-[15px]'>
         <a href="/" className='text-[28px] font-bold text-green-900'>Trip <span className='text-[#FFDF00]'>Makers</span></a>
         <ul className='hidden lg:flex gap-[30px] cursor-pointer text-[19px] font-semibold rubik'>
-          {['home', 'about', 'destin', 'blog', 'contact'].map((item, index) => (
-            <li key={index} className='relative group'>
-              <Link to={item} smooth={true} offset={-150} duration={700}>{item.charAt(0).toUpperCase() + item.slice(1)}</Link>
-              <span className='absolute bottom-0 left-0 w-full h-[2px] bg-[#FFDF00] transform scale-x-0 group-hover:scale-x-100 transition-transform duration-300 ease-in-out'></span>
-            </li>
-          ))}
+          <li className='relative group'>
+            <Link to='home' smooth={true} offset={-150} duration={700}>Home</Link>
+            <span className='absolute bottom-0 left-0 w-full h-[2px] bg-[#FFDF00] transform scale-x-0 group-hover:scale-x-100 transition-transform duration-300 ease-in-out'></span>
+          </li>
+          <li className='relative group'>
+            <Link to='about' smooth={true} offset={0} duration={700}>About</Link>
+            <span className='absolute bottom-0 left-0 w-full h-[2px] bg-[#FFDF00] transform scale-x-0 group-hover:scale-x-100 transition-transform duration-300 ease-in-out'></span>
+          </li>
+          <li className='relative group'>
+            <Link to='destin' smooth={true} offset={0} duration={700}>Destinations</Link>
+            <span className='absolute bottom-0 left-0 w-full h-[2px] bg-[#FFDF00] transform scale-x-0 group-hover:scale-x-100 transition-transform duration-300 ease-in-out'></span>
+          </li>
+          <li className='relative group'>
+            <Link to='blog' smooth={true} offset={-40} duration={700}>Blog</Link>
+            <span className='absolute bottom-0 left-0 w-full h-[2px] bg-[#FFDF00] transform scale-x-0 group-hover:scale-x-100 transition-transform duration-300 ease-in-out'></span>
+          </li>
+          <li className='relative group'>
+            <Link to='contact' smooth={true} offset={-30} duration={700}>Contact</Link>
+            <span className='absolute bottom-0 left-0 w-full h-[2px] bg-[#FFDF00] transform scale-x-0 group-hover:scale-x-100 transition-transform duration-300 ease-in-out'></span>
+          </li>
         </ul>
         <div onClick={toggleNav} className='lg:hidden z-30'>
           {nav ? <AiOutlineClose size={30} /> : <AiOutlineMenu size={30} />}
         </div>
         <div className={nav ? 'text-center fixed h-full w-full left-0 top-0 bg-green-900' : 'fixed left-[-100%] transition-all duration-300'}>
           <ul className='font-semibold text-[22px] space-y-8 mt-24 rubik text-white'>
-            {['home', 'about', 'destin', 'blog', 'contact'].map((item, index) => (
-              <li key={index}><Link to={item} smooth={true} offset={-150} duration={500} onClick={closeNav}>{item.toUpperCase()}</Link></li>
-            ))}
+            <li><Link to='home' smooth={true} offset={-150} duration={500} onClick={closeNav}>HOME</Link></li>
+            <li><Link to='about' smooth={true} offset={0} duration={500} onClick={closeNav}>ABOUT</Link></li>
+            <li><Link to='destin' smooth={true} offset={0} duration={500} onClick={closeNav}>DESTINATIONS</Link></li>
+            <li><Link to='blog' smooth={true} offset={-40} duration={500} onClick={closeNav}>BLOG</Link></li>
+            <li><Link to='contact' smooth={true} offset={-30} duration={500} onClick={closeNav}>CONTACT</Link></li>
           </ul>
         </div>
       </div>
